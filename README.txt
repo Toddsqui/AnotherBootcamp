3cfae64f934e8c3920a898e1209fe95bb5630ae0
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2016-05-12 11:00:11 +0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+Code forked from: http://codepen.io/zeckdude/pen/KVaJLE.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Flexbox Resources
+
+// http://www.sketchingwithcss.com/samplechapter/cheatsheet.html
+
+// https://css-tricks.com/snippets/css/a-guide-to-flexbox/
